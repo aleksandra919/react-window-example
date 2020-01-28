0fd637cdf41351a0f8f9f7bc6d23c7d18a2969b1
Aleksandra Rolka <aleksandrarolka91@gmail.com>
2020-01-28 22:02:49 +0100

    added react-window-example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const generateItems = numItems =>
+  Array(numItems)
+    .fill(true)
+    .map(_ => ({
+      key: Math.round(Math.random() * 10),
+      label: Math.random()
+        .toString(36)
+        .substr(2),
+    }));
+
+    const Row = ({ data, index, style }) => {
+      // Data passed to List as "itemData" is available as props.data
+      const item = data[index]
+      return (
+        <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+          Label: {item.label}, key:  {item.key} 
+        </div>
+      );
+    }
+
+
+export default () => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <List
+        className="List"
+        itemCount={1000}
+        itemSize={35}
+        height={500}
+        width={500}
+        itemData={generateItems(100)}
+      >
+        {Row}
+      </List>
+    )}
+  </AutoSizer>
+  )
